32a32f559d1d97c1026d98e15848739c70dc190b
김동균 <wooheuy5752@nate.com>
2024-06-26 14:52:56 +0900

    FEAT : 메인페이지 아이템 요소 구현

---
+++ src/app/components/MainBalanceItem.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MainBalanceItem = () => {
+  return (
+    <div className="flexBetweenCenter gap-3 rounded-lg bg-blue-100 px-5 py-1">
+      <p className="flex-1 overflow-hidden text-ellipsis whitespace-nowrap text-xs">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+      <p className="text-2xl font-bold text-gray-200">VS</p>
+      <p className="flex-1 overflow-hidden text-ellipsis whitespace-nowrap text-xs">item</p>
+    </div>
+  );
+};
+
+export default MainBalanceItem;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import MainBalanceItem from './components/MainBalanceItem';
 import NavHeader from './components/NavHeader';
 
 export default function Home() {
   return (
     <div className="gap-4 rounded-lg bg-white px-2 py-3">
       <NavHeader>최고 인기</NavHeader>
-      <div>box</div>
+      <MainBalanceItem />
     </div>
   );
 }
