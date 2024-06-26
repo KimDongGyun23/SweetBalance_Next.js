dcdd3e3dca5322fd98776045b8d467b4fc7257e0
김동균 <wooheuy5752@nate.com>
2024-06-26 20:53:26 +0900

    CHORE : 메인페이지 item 글자 가운데 정렬

--- src/app/components/MainBalanceItem.tsx
+++ src/app/components/MainBalanceItem.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MainBalanceItem = () => {
   return (
-    <div className="flexBetweenCenter gap-3 rounded-lg bg-blue-100 px-5 py-1">
+    <div className="flexBetweenCenter gap-3 rounded-lg bg-blue-100 px-5 py-1 text-center">
       <p className="flex-1 overflow-hidden text-ellipsis whitespace-nowrap text-xs">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
       </p>
