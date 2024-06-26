ee0babcf5f0d6ce259ea9ae4ab75ede0e0a24333
김동균 <wooheuy5752@nate.com>
2024-06-26 15:37:15 +0900

    CHORE : NavHeader 폰트 설정 이동

--- src/app/components/NavHeader.tsx
+++ src/app/components/NavHeader.tsx
@@ -3,8 +3,8 @@ import { AiOutlineRight } from 'react-icons/ai';
 
 const NavHeader = ({ children }: ChildrenProps) => {
   return (
-    <div className="flexAlign mb-4 gap-1 text-xl font-semibold">
-      <div>{children}</div>
+    <div className="flexAlign mb-4 gap-1">
+      <div className="text-xl font-semibold">{children}</div>
       <AiOutlineRight size={19} />
     </div>
   );
