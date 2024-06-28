e989eaf6f40f59ab742806ba4d34f42eaf1b6ef0
김동균 <wooheuy5752@nate.com>
2024-06-28 11:38:39 +0900

    CHORE : 폼 제출 로직 분리

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -10,15 +10,13 @@ import { useRouter } from 'next/navigation';
 const Register = () => {
   const router = useRouter();
   const { register, handleSubmit, errors } = useRegisterForm();
+  const handleSubmitForm = async (formData: any) => {
+    const GAME_ID = await createBalance(formData);
+    router.push(`/balance/${GAME_ID}`);
+  };
 
   return (
-    <form
-      className="px-4 py-5"
-      onSubmit={handleSubmit(async (formData) => {
-        const GAME_ID = await createBalance(formData);
-        router.push(`/balance/${GAME_ID}`);
-      })}
-    >
+    <form className="px-4 py-5" onSubmit={handleSubmit(handleSubmitForm)}>
       <div className="mb-5">
         <BalanceRegisterItem side="left" register={register} errors={errors} />
         <Image src="/svgs/verse.svg" className="mx-auto" alt="vs" width={64} height={96} />
