478441e956c7780577964cc7767036afbdfe8242
김동균 <wooheuy5752@nate.com>
2024-06-29 14:46:08 +0900

    FEAT : 동그라미 버튼 라우팅 설정

--- src/components/CircleButton.tsx
+++ src/components/CircleButton.tsx
@@ -1,8 +1,15 @@
+'use client';
 import { ChildrenProps } from '@/types/commonType';
+import { useRouter } from 'next/navigation';
 
 const CircleButton = ({ children }: ChildrenProps) => {
+  const router = useRouter();
+
   return (
-    <div className="flexCenter fixed bottom-10 h-20 w-20 cursor-pointer self-end rounded-full border-4 border-blue-400 bg-white">
+    <div
+      className="flexCenter fixed bottom-10 h-20 w-20 cursor-pointer self-end rounded-full border-4 border-blue-400 bg-white"
+      onClick={() => router.push('/register')}
+    >
       <p className="text-lg font-bold text-blue-400">{children}</p>
     </div>
   );
