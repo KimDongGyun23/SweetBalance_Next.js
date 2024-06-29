207a7536d2c15fa11e20a8f69c7a288663558d00
김동균 <wooheuy5752@nate.com>
2024-06-29 11:09:12 +0900

    REFACTOR : changeSubjectApi 이름 변경 및 하위 로직 언어 변경으로 바꿈

---
+++ src/utils/changeSubjectLang.ts
@@ -0,0 +1,19 @@
+import { SUBJECT_EN, SUBJECT_KR } from '@/constants/commonet';
+
+const changeSubjectLang = (subject: string) => {
+  console.log(subject);
+  let returnArr = [];
+  let returnIndex = 0;
+
+  const regex = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+  if (regex.test(subject)) {
+    returnArr = [...SUBJECT_EN];
+    returnIndex = SUBJECT_KR.indexOf(subject);
+  } else {
+    returnArr = [...SUBJECT_KR];
+    returnIndex = SUBJECT_EN.indexOf(subject);
+  }
+  return returnArr[returnIndex];
+};
+
+export default changeSubjectLang;
