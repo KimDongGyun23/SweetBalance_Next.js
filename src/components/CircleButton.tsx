08539d9b0c066c082ee6665eb11f59140ec3b0c4
김동균 <wooheuy5752@nate.com>
2024-06-29 14:45:09 +0900

    STYLE : 동그라미 버튼 cursor pointer 설정

--- src/components/CircleButton.tsx
+++ src/components/CircleButton.tsx
@@ -2,7 +2,7 @@ import { ChildrenProps } from '@/types/commonType';
 
 const CircleButton = ({ children }: ChildrenProps) => {
   return (
-    <div className="flexCenter fixed bottom-10 h-20 w-20 self-end rounded-full border-4 border-blue-400 bg-white">
+    <div className="flexCenter fixed bottom-10 h-20 w-20 cursor-pointer self-end rounded-full border-4 border-blue-400 bg-white">
       <p className="text-lg font-bold text-blue-400">{children}</p>
     </div>
   );
