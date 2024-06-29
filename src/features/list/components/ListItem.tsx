de2c8a01c7cd2ee0ac96a3d4fb104e736649ac41
김동균 <wooheuy5752@nate.com>
2024-06-29 11:37:59 +0900

    FEAT : 리스트 페이지에서 밸런스 페이지로 라우팅 설정

--- src/features/list/components/ListItem.tsx
+++ src/features/list/components/ListItem.tsx
@@ -1,8 +1,16 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import { AiOutlineInbox } from 'react-icons/ai';
 
 const ListItem = ({ postItem }: { postItem: any }) => {
+  const router = useRouter();
+  const handleClick = () => router.push(`/balance/${postItem.id}`);
+
   return (
-    <div className="flexBetweenCenter relative gap-3 rounded-lg bg-white px-6 py-4">
+    <div
+      className="flexBetweenCenter relative cursor-pointer gap-3 rounded-lg bg-white px-6 py-4"
+      onClick={handleClick}
+    >
       <p className="flex-1 overflow-hidden text-ellipsis whitespace-nowrap text-center text-xl">
         {postItem.leftSideTitle}
       </p>
