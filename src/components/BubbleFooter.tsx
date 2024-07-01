f260ade93318907488488867cc8f2b4aa5a944c2
김동균 <wooheuy5752@nate.com>
2024-07-01 17:18:04 +0900

    refactor : 대댓글 버튼 클릭 로직 수정

--- src/components/BubbleFooter.tsx
+++ src/components/BubbleFooter.tsx
@@ -1,11 +1,11 @@
 'use client';
 import { BubbleFooterType } from '@/types/commentType';
-import { usePathname, useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { AiOutlineComment, AiOutlineHeart } from 'react-icons/ai';
 
 const BubbleFooter = ({ commentId, comment }: BubbleFooterType) => {
   const path = usePathname();
-  const router = useRouter();
   const isRecomment = path.includes('recomment');
   const { id: recommentId, sideInfo, likeCount, childCount } = comment;
 
@@ -14,13 +14,12 @@ const BubbleFooter = ({ commentId, comment }: BubbleFooterType) => {
       className={`flex ${sideInfo === 0 ? 'justify-end' : 'justify-start'} gap-[7px] text-xs text-gray-100`}
     >
       {!isRecomment && (
-        <div
-          className="flexAlign cursor-pointer gap-[1px]"
-          onClick={() => router.push(`/comment/${commentId}/recomment/${recommentId}`)}
-        >
-          <span>{childCount}</span>
-          <AiOutlineComment />
-        </div>
+        <Link href={`/comment/${commentId}/recomment/${recommentId}`}>
+          <div className="flexAlign cursor-pointer gap-[1px]">
+            <span>{childCount}</span>
+            <AiOutlineComment />
+          </div>
+        </Link>
       )}
 
       <div className="flexAlign cursor-pointer gap-[1px]">
